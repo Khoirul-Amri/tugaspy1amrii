014f2b3a3dc389bae092ebc0bafe958999c62689
Khoirul Amri <khoirulamri.tro25@poltera.ac.id>
2026-09-04 15:22:33 +0700

    Menambahkan info kelas pada program hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ import sys
 print("Praktek Pemrograman Terapan - Pertemuan 1")
 print("Nama :", "___Khoirul Amri___")
 print("NIM :", "___33132501003___")
+print("Kelas :", "___TRO IIIA TRO___")
 print("Python:", sys.version)
