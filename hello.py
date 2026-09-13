cc0aedee11a3445c0287bf9c70959c8a2c82e8c5
Khoirul Amri <khoirulamri.tro25@poltera.ac.id>
2026-09-14 01:48:13 +0700

    Tambah identitas kelompok pada hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
-import sys
-print("Praktek Pemrograman Terapan - Pertemuan 1")
-print("Nama :", "___Khoirul Amri___")
-print("NIM :", "___33132501003___")
-print("Kelas :", "___TRO IIIA TRO___")
-print("Jenis Kelamin :", "___Laki-laki___")
-print("Alamat :", "___Jl Syamsul Arifin, Sampang, Madura___")
-print("Python:", sys.version)
-print("Halo dari praktikum Git dan GitHub!")
+print("=== DATA ANGGOTA KELOMPOK ===")
+print("1. KHoirul Amri - NRP: 33132501003")
+print("2. Anggota Kelompok 2")
