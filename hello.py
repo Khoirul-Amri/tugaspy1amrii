803836a0ba7b1fe38728ceb1004d79a12ea194ca
Khoirul Amri <khoirulamri.tro25@poltera.ac.id>
2026-09-14 03:19:07 +0700

    TAMBAHKAN DAFTAR KELOMPOK

--- hello.py
+++ hello.py
@@ -7,3 +7,9 @@ print("NRP          : 33132501003")
 print("Kelas        : 1A TRO")
 print("Jenis Kelamin: Laki-laki")
 print("Alamat       : Sampang, Madura")
+
+# Program hello.py - Identitas Kelompok
+print("=== DAFTAR ANGGOTA KELOMPOK ===")
+print("Kelompok : Kelompok 1")
+print("1. KHoirul Amri (33132501003) - D4 TRO")
+print("2. Khoirun NISA (33132501004) - D4 TRO")
